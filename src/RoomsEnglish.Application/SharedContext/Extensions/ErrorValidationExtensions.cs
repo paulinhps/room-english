a2be22ac690534ec71bd3ef93359bf07933696aa
Paulinho Santana <paulo.santana@montrealviagens.com>
2024-01-30 16:16:18 -0300

    feat: include a fluent validation failure extensions converter

---
+++ src/RoomsEnglish.Application/SharedContext/Extensions/ErrorValidationExtensions.cs
@@ -0,0 +1,16 @@
+using FluentValidation.Results;
+using RoomsEnglish.Domain.SharedContext.Models;
+
+namespace RoomsEnglish.Application.SharedContext.Extensions;
+
+public static class ErrorValidationExtensions
+{
+    public static Notification[] GetNotifications(this IEnumerable<ValidationFailure> validationFailures)
+    {
+        return validationFailures.Select(failure => new Notification(failure.PropertyName, failure.ErrorMessage)).ToArray();
+    }
+    public static Error[] GetErrors(this IEnumerable<ValidationFailure> validationFailures)
+    {
+        return validationFailures.Select(failure => new Error(failure.PropertyName, failure.ErrorMessage)).ToArray();
+    }
+}
