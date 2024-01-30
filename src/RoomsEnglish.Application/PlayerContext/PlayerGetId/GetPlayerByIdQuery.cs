85bdce32af9779c71e57eb5072b3e7ffb7cc0f3e
Allan Debastiani <allandeba@yahoo.com.br>
2024-01-29 23:33:56 -0300

    feat Player minimal controller

---
+++ src/RoomsEnglish.Application/PlayerContext/PlayerGetId/GetPlayerByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using RoomsEnglish.Application.PlayerContext.ViewModels;
+
+namespace RoomsEnglish.Application.PlayerContext.PlayerGetId;
+public class GetPlayerByIdQuery : IRequest<QueryResult<PlayerViewModel>>
+{
+    public Guid Id { get; set; }
+}
