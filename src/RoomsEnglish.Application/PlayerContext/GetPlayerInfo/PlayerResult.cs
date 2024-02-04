0cb0e1d07a28951c990ecebe843fe5ae9d26ff8e
Allan Debastiani <allandeba@yahoo.com.br>
2024-02-04 20:14:57 -0300

    feat GetPlayers
    Não sei por que cargas d'agua isso voltou a ser assim... No commit anterior foi alterado.

--- src/RoomsEnglish.Application/PlayerContext/GetPlayerInfo/PlayerResult.cs
+++ src/RoomsEnglish.Application/PlayerContext/GetPlayerInfo/PlayerResult.cs
@@ -1,10 +1,17 @@
-namespace RoomsEnglish.Application.PlayerContext.GetPlayerInfo;
+using RoomsEnglish.Application.PlayerContext.ViewModels;
+
+namespace RoomsEnglish.Application.PlayerContext.GetPlayers;
 
 public class QueryResult<T>
 {
-    public IEnumerable<T>? Data { get; set; }
-    public bool Success => !Errors.Any() || MessageCode != 200;
+    public T? Data { get; set; }
+    public bool Success => !Errors.Any() && MessageCode < 400;
     public string? Message { get; set; }
     public int MessageCode { get; set; } = 200;
     public IEnumerable<string> Errors { get; set; } = new List<string>();
+
+    public static implicit operator QueryResult<T>(QueryResult<IEnumerable<PlayerViewModel>> v)
+    {
+        throw new NotImplementedException();
+    }
 }
